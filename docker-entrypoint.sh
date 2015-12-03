b0e55804db57a20acf31e4b07711baa9c135b1b0
Adam Craven <adammcraven@gmail.com>
2015-12-04 00:03:23 +1000

    wip

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,19 +6,35 @@ set -ex
 validate_variables() {
 	
 	if [ ! $DB_SERVER_ADDRESSES ]; then
-	echo "==> ERROR: DB_SERVER_ADDRESSES was not supplied"
-	exit -1
+	  echo "==> ERROR: DB_SERVER_ADDRESSES was not supplied"
+	  exit -1
 	fi
 	
-	if [ ! $GALERAMON_USER ]; then
-	echo "==> ERROR: GALERAMON_USER was not supplied"
-	exit -1
+	if [ ! $DB_SERVER_PORTS ]; then
+	  echo "==> ERROR: DB_SERVER_PORTS was not supplied"
+	  exit -1
+	fi	
+	
+	if [ ! $MAXSCALE_USER ]; then
+	  echo "==> ERROR: MAXSCALE_USER was not supplied"
+	  exit -1
 	fi
 	
-	if [ ! $GALERAMON_PASSWORD ]; then
-	echo "==> ERROR: GALERAMON_PASSWORD was not supplied"
-	exit -1
+	if [ ! $MAXSCALE_PASSWORD ]; then
+	  echo "==> ERROR: MAXSCALE_PASSWORD was not supplied"
+	  exit -1
 	fi
+	
+	if [ ! $MONITOR_USER ]; then
+	  echo "==> ERROR: MONITOR_USER was not supplied"
+	  exit -1
+	fi
+	
+	if [ ! $MONITOR_PASSWORD ]; then
+	  echo "==> ERROR: MONITOR_PASSWORD was not supplied"
+	  exit -1
+	fi
+	
 }
 
 function joinArrayToString { local IFS="$1"; shift; echo "$*"; }
@@ -26,22 +42,28 @@ function joinArrayToString { local IFS="$1"; shift; echo "$*"; }
 create_config_file() {
 
     dbServers=()
+	dbServerPorts=()
 
 	# Split comma separated list of database server addresses
 	IFS=',' read -r -a dbServerAddresses <<< "$DB_SERVER_ADDRESSES"
+    IFS=',' read -r -a dbServerPorts <<< "$DB_SERVER_PORTS"
+
 	for index in "${!dbServerAddresses[@]}"
 	do
 	  position = ${index+1}
 	  
       ipAddress = ${dbServerAddresses[index]} | sed 's/[[:space:]]//g'
-      addressHostName = "address${position}"
+      #addressHostName = "address${position}"
       # echo $address1  address1 >> /etc/hosts
-      echo $ipAddress  $addressHostName >> /etc/hosts
+      #echo $ipAddress  $addressHostName >> /etc/hosts
 
-	  dbServers+=("dbserv${position}")	  
+	  dbServers+=("server${position}")	  
     done
 	
 	commaSeparatedDbServers=joinArrayToString , "${dbServers[@]}"
+	
+	# Reference:
+	# https://mariadb.com/kb/en/mariadb-enterprise/mariadb-maxscale/maxscale-configuration-usage-scenarios/
 	
 	echo "==> Creating maxscale config file"
 	maxscaleConf="/etc/maxscale.d/maxscale.cnf"
@@ -56,15 +78,16 @@ create_config_file() {
 	
 	[Splitter Service]
 	type=service
-	router=readwritesplit
-	servers=$commaSeparatedDbServers
+	router=readconnroute
+	router_options=master,slave
+	***TODO servers=$commaSeparatedDbServers
 	
 	[Splitter Service]
 	type=service
 	router=readwritesplit
-	servers=$commaSeparatedDbServers
-	user=$GALERAMON_USER
-	passwd=$GALERAMON_PASSWORD
+	***TODO servers=$commaSeparatedDbServers
+	user=$MAXSCALE_USER
+	passwd=$MAXSCALE_PASSWORD
 	
 	[Splitter Listener]
 	type=listener
@@ -72,42 +95,60 @@ create_config_file() {
 	protocol=MySQLClient
 	port=3306
 	socket=/tmp/ClusterMaster
-EOM
-	) >> $maxscaleConf
-
-
-
-TODO	
-	# Do a loop here instead
-	( cat <<EOM
-	[dbserv1]
-	type=server
-	address=address1
-	port=3306
-	protocol=MySQLBackend
 	
-	[dbserv2]
-	type=server
-	address=address2
-	port=3306
-	protocol=MySQLBackend
-	
-	[dbserv3]
-	type=server
-	address=address3
-	port=3306
-	protocol=MySQLBackend
 	
 EOM
 	) >> $maxscaleConf
+
+
+#ssl=required
+#ssl_cert=/home/markus/certs/server-cert.pem
+#ssl_key=/home/markus/certs/server-key.pem
+#ssl_ca_cert=/home/markus/certs/ca.pem
+#ssl_version=TLSv12	
+
+
+	for index in "${!dbServerAddresses[@]}"
+	do
+	  position = ${index+1}
+      ipAddress = ${dbServerAddresses[index]} | sed 's/[[:space:]]//g'
+	  port = ${dbServerPorts[index]} | sed 's/[[:space:]]//g'
+	
+	  ( cat <<EOM
+		[server$position]
+		type=server
+		address=$ipAddress
+		port=$port
+		# lowest index server, if available, becomes Master
+		wsrep_local_index=$position
+		protocol=MySQLBackend
+		monitoruser=$MONITOR_USER
+		monitorpw=$MONITOR_PASSWORD
+EOM
+	  ) >> $maxscaleConf
+    done
+
+
+
 	
 	( cat <<EOM
 	[Galera Monitor]
 	type=monitor
 	module=galeramon
 	servers=$commaSeparatedDbServers
-	user=$GALERAMON_USER
-	passwd=$GALERAMON_PASSWORD
+	user=$MONITOR_USER
+	passwd=$MONITOR_PASSWORD
+	monitor_interval=8000
+	backend_connect_timeout=3
+	backend_read_timeout=1
+	backend_write_timeout=2
+	# galeramon specific options
+	disable_master_failback=0
+	available_when_donor=0
+	# 0 = set servers to operate as master or slave 
+	disable_master_role_setting=0
+	
+	
 	
 	[CLI]
 	type=service
